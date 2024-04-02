c59ac2f92b3f608115a041bb6ddbd128d35d1855
Dinh Tran <trd@umbraco.dk>
2024-04-02 13:44:44 +0700

    Improve validation

--- src/Umbraco.Commerce.ProductFeeds.Infrastructure/DtoMappings/InfrastructureMappingProfile.cs
+++ src/Umbraco.Commerce.ProductFeeds.Infrastructure/DtoMappings/InfrastructureMappingProfile.cs
@@ -12,7 +12,9 @@ namespace Umbraco.Commerce.ProductFeeds.Infrastructure.DtoMappings
         {
             CreateMap<ProductFeedSettingWriteModel, UmbracoCommerceProductFeedSetting>(MemberList.Source)
                 .ForSourceMember(src => src.PropertyNameMappings, opt => opt.DoNotValidate())
-                .ForMember(dest => dest.ProductPropertyNameMappings, opt => opt.MapFrom((src, dest) => JsonSerializer.Serialize(src.PropertyNameMappings)));
+                .ForMember(dest => dest.ProductPropertyNameMappings, opt => opt.MapFrom((src, dest) => JsonSerializer.Serialize(src.PropertyNameMappings)))
+                .ForSourceMember(src => src.ProductDocumentTypeAliases, opt => opt.DoNotValidate())
+                .ForMember(dest => dest.ProductDocumentTypeAliases, opt => opt.MapFrom((src, dest) => string.Join(';', src.ProductDocumentTypeAliases)));
 
             CreateMap<UmbracoCommerceProductFeedSetting, ProductFeedSettingReadModel>()
                 .ForMember(dest => dest.PropertyNameMappings, opt => opt.MapFrom((src, dest) => JsonSerializer.Deserialize<ICollection<PropertyValueMapping>>(src.ProductPropertyNameMappings)))
