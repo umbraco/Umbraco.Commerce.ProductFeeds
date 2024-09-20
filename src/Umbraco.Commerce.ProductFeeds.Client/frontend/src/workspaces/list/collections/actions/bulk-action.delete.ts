62edcc1fdd676eb4f1d9a88e6a26bc92d644a2b5
Dinh Tran <trd@umbraco.dk>
2024-09-20 14:15:59 +0700

    v14: make bulk delete button work

--- src/Umbraco.Commerce.ProductFeeds.Client/frontend/src/workspaces/list/collections/actions/bulk-action.delete.ts
+++ src/Umbraco.Commerce.ProductFeeds.Client/frontend/src/workspaces/list/collections/actions/bulk-action.delete.ts
@@ -27,7 +27,7 @@ export class UcpfListBulkDeleteAction extends UmbEntityBulkActionBase<MetaEntity
 
             try {
                 await umbConfirmModal(this._host, {
-                    headline: 'UCPF Delete Item',
+                    headline: 'Delete item',
                     content: `Are you sure you want to delete ${this.selection.length} items?`,
                     color: 'danger',
                     confirmLabel: 'Delete',
@@ -40,29 +40,22 @@ export class UcpfListBulkDeleteAction extends UmbEntityBulkActionBase<MetaEntity
             const notificationContext = await this.getContext(UMB_NOTIFICATION_CONTEXT);
             const localize = new UmbLocalizationController(this._host);
             if (isSuccess) {
-
                 const entityContext = await this.getContext(UMB_ENTITY_CONTEXT);
                 if (!entityContext) throw new Error('Entity Context is not available');
-
-                const entityType = entityContext.getEntityType();
-                const unique = entityContext.getUnique();
 
                 const eventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
                 if (!eventContext) throw new Error('Event Context is not available');
                 const event = new UmbRequestReloadChildrenOfEntityEvent({
-                    // entityType: listingWorkspaceManifest.meta.entityType,
-                    entityType: entityType!,
-                    unique: unique!,
-                    // unique: null,
+                    entityType: entityContext.getEntityType()!,
+                    unique: null,
                 });
 
                 eventContext.dispatchEvent(event);
-                console.log('bulk delete finished');
-                // notificationContext.peek('positive', {
-                //     data: {
-                //         message: localize.term('ucProductFeeds_messageDeleteSuccess') ?? 'Delete successfully',
-                //     },
-                // });
+                notificationContext.peek('positive', {
+                    data: {
+                        message: localize.term('ucProductFeeds_messageDeleteSuccess') ?? 'Delete successfully',
+                    },
+                });
             } else {
                 notificationContext.peek('danger', {
                     data: {
--- src/Umbraco.Commerce.ProductFeeds.Client/frontend/src/workspaces/list/context.ts
+++ src/Umbraco.Commerce.ProductFeeds.Client/frontend/src/workspaces/list/context.ts
@@ -1,8 +1,10 @@
 import { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
-import { UmbDefaultWorkspaceContext, UmbRoutableWorkspaceContext, UmbWorkspaceContext, UmbWorkspaceRouteManager } from '@umbraco-cms/backoffice/workspace';
+import { UmbDefaultWorkspaceContext, UmbEntityWorkspaceContext, UmbRoutableWorkspaceContext, UmbWorkspaceContext, UmbWorkspaceRouteManager, UmbWorkspaceUniqueType } from '@umbraco-cms/backoffice/workspace';
 import UcpfListWorkspaceElement from './index.element.js';
 import { listingWorkspaceManifest } from './manifests.js';
+import { UmbBasicState } from '@umbraco-cms/backoffice/observable-api';
+import { UmbEntityContext } from '@umbraco-cms/backoffice/entity';
 
 export const LISTING_WORKSPACE_CONTEXT = new UmbContextToken<
     UmbWorkspaceContext,
@@ -15,11 +17,21 @@ export const LISTING_WORKSPACE_CONTEXT = new UmbContextToken<
 
 export class UcpfListListingWorkspaceContext
     extends UmbDefaultWorkspaceContext
-    implements UmbWorkspaceContext, UmbRoutableWorkspaceContext {
+    implements UmbWorkspaceContext, UmbRoutableWorkspaceContext, UmbEntityWorkspaceContext {
     readonly routes = new UmbWorkspaceRouteManager(this);
+
+    #entityContext: UmbEntityContext = new UmbEntityContext(this);
+
+    #entityType: UmbBasicState<string | undefined> = new UmbBasicState<string | undefined>(undefined);
+    readonly entityType = this.#entityType.asObservable();
+
+    #unique: UmbBasicState<UmbWorkspaceUniqueType | undefined> = new UmbBasicState<UmbWorkspaceUniqueType | undefined>(undefined);
+    readonly unique = this.#unique.asObservable();
 
     constructor(host: UmbControllerHost) {
         super(host);
+        this.observe(this.entityType, (entityType) => this.#entityContext!.setEntityType(entityType));
+        this.observe(this.unique, (unique) => this.#entityContext.setUnique(unique ?? null));
         this.routes.setRoutes([
             {
                 path: '',
