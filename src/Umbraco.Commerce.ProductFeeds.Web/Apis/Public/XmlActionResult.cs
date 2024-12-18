ba7ad22add76b71cb37e153fe8ae847903042503
Dinh Tran <trd@umbraco.dk>
2024-12-18 18:23:49 +0700

    Cherry-pick #21: Format the returned XML because facebook limits the length of one row (#22)
    * Format the returned XML because facebook limits the length of one row (#21)
    
    Co-authored-by: Tim Gaunt <tim@tsd.digital>
    
    * minor changes to code format and bump version
    
    * switch to synchronous Flush because XmlTextWriter.FlushAsync throws NotImplementedException
    
    ---------
    
    Co-authored-by: Tim Gaunt <timgaunt@users.noreply.github.com>
    Co-authored-by: Tim Gaunt <tim@tsd.digital>

---
+++ src/Umbraco.Commerce.ProductFeeds.Web/Apis/Public/XmlActionResult.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Umbraco.Commerce.ProductFeeds.Apis.Public;
+
+public sealed class XmlActionResult : IActionResult
+{
+    private static readonly Serilog.ILogger _logger = Serilog.Log.Logger.ForContext(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    private readonly XmlDocument _document;
+
+    public Formatting Formatting { get; set; } = Formatting.None;
+    public string MimeType { get; set; } = "text/xml";
+
+    public XmlActionResult(XmlDocument document)
+    {
+        ArgumentNullException.ThrowIfNull(document);
+        _document = document;
+    }
+
+    public async Task ExecuteResultAsync(ActionContext context)
+    {
+        context.HttpContext.Response.Clear();
+        context.HttpContext.Response.ContentType = MimeType;
+
+        try
+        {
+            using (var writer = new XmlTextWriter(context.HttpContext.Response.Body, Encoding.UTF8))
+            {
+                writer.Formatting = Formatting;
+                _document.WriteContentTo(writer);
+                writer.Flush();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Exception generating the XML response");
+        }
+    }
+}
