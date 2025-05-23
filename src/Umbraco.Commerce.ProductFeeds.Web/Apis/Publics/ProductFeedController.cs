ec93a3146b7cd4ac6fda11c53d5fd06b593ca2eb
Ben White <b.white@rocburn.com>
2025-05-23 15:55:04 +0100

    Provide a mechanism for IProductFeedGeneratrorService implementations to declare the format they generate.  Use this format declaration to return the correct output within ProductFeedController.

--- src/Umbraco.Commerce.ProductFeeds.Web/Apis/Publics/ProductFeedController.cs
+++ src/Umbraco.Commerce.ProductFeeds.Web/Apis/Publics/ProductFeedController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +20,7 @@ namespace Umbraco.Commerce.ProductFeeds.Web.Apis.Publics
             _feedConfigService = feedConfigService;
         }
 
-        public async Task<IActionResult> Xml(string path)
+        public async Task<IActionResult> Generate(string path)
         {
             ProductFeedSettingReadModel? feedSettings = await _feedConfigService
                 .FindSettingAsync(new FindSettingParams { FeedRelativePath = path })
@@ -30,10 +31,20 @@ namespace Umbraco.Commerce.ProductFeeds.Web.Apis.Publics
             }
 
             IProductFeedGeneratorService feedGenerator = _feedGeneratorFactory.GetGenerator(feedSettings.FeedGeneratorId);
-            XmlDocument feed = await feedGenerator.GenerateFeedAsync(feedSettings);
 
-            var result = new XmlActionResult(feed) { Formatting = Formatting.Indented };
-            return result;
+            switch (feedGenerator.Format)
+            {
+                case FeedFormat.Xml:
+                    XmlDocument xmlFeed = await feedGenerator.GenerateXmlFeedAsync(feedSettings);
+                    var result = new XmlActionResult(xmlFeed) { Formatting = Formatting.Indented };
+                    return result;
+                case FeedFormat.Json:
+                    JsonDocument jsonFeed = await feedGenerator.GenerateJsonFeedAsync(feedSettings);
+                    var jsonResult = new JsonResult(jsonFeed.RootElement);
+                    return jsonResult;
+                default:
+                    return Problem("Unknown feed format.");
+            }
         }
     }
 }
