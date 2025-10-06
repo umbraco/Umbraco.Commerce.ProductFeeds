cb440c15c089af68278c64a567dfb909e6f0d458
Dinh Tran <trd@umbraco.dk>
2025-10-06 12:47:00 +0700

    fix DefaultMultipleMediaPickerPropertyValueExtractor unable to cast exception

--- src/Umbraco.Commerce.ProductFeeds.Core/Features/PropertyValueExtractors/Implementations/DefaultMultipleMediaPickerPropertyValueExtractor.cs
+++ src/Umbraco.Commerce.ProductFeeds.Core/Features/PropertyValueExtractors/Implementations/DefaultMultipleMediaPickerPropertyValueExtractor.cs
@@ -14,18 +14,17 @@ namespace Umbraco.Commerce.ProductFeeds.Core.Features.PropertyValueExtractors.Im
         /// <inheritdoc/>
         public ICollection<string> Extract(IPublishedElement content, string propertyAlias, IPublishedElement? fallbackElement)
         {
-            if (!content.HasProperty(propertyAlias))
+            IEnumerable<IPublishedContent>? items = content.GetPropertyValue<IEnumerable<IPublishedContent>>(propertyAlias, fallbackElement);
+            if (items == null)
             {
-                return new List<string>();
+                return [];
             }
 
-            List<MediaWithCrops>? medias = content.GetPropertyValue<List<MediaWithCrops>>(propertyAlias, fallbackElement);
-            if (medias == null || medias.Count == 0)
-            {
-                return new List<string>();
-            }
-
-            return medias.Select(x => x.MediaUrl(mode: UrlMode.Absolute)).ToList();
+            return items
+                .Where(x => x != null)
+                .Select(x => x.MediaUrl(mode: UrlMode.Absolute))
+                .ToList();
         }
+
     }
 }
