44ac8cb3d735855f8f2ce4dda2178e854f2a12cb
mattbrailsford <me@mattbrailsford.com>
2025-11-13 14:59:51 +0000

    Compile against RC2

--- tests/Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests/AutoMapperTests.cs
+++ tests/Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests/AutoMapperTests.cs
@@ -1,16 +1,17 @@
 using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Umbraco.Commerce.ProductFeeds.Infrastructure.DtoMappings;
 
 namespace Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests
 {
 #pragma warning disable CA1515 // Consider making public types internal
-    public class AutoMapperTests
+    public class AutoMapperTests(ILoggerFactory loggerFactory)
 #pragma warning restore CA1515 // Consider making public types internal
     {
         [Fact]
         public void Mapping_Configuration_Should_Be_Valid()
         {
-            MapperConfiguration config = new(cfg => cfg.AddMaps(typeof(InfrastructureMappingProfile)));
+            MapperConfiguration config = new(cfg => cfg.AddMaps(typeof(InfrastructureMappingProfile)), loggerFactory);
             config.AssertConfigurationIsValid();
         }
     }
