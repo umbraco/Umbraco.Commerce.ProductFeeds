d621ccadb4a62992a5d188002bed82a24300e0bc
Dinh Tran <trd@umbraco.dk>
2025-11-26 15:23:33 +0700

    Fix failing tests after upgrading AutoMapper to 15

--- tests/Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests/AutoMapperTests.cs
+++ tests/Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests/AutoMapperTests.cs
@@ -1,17 +1,21 @@
 using AutoMapper;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Umbraco.Commerce.ProductFeeds.Infrastructure.DtoMappings;
 
 namespace Umbraco.Commerce.ProductFeeds.Infrastructure.UnitTests
 {
 #pragma warning disable CA1515 // Consider making public types internal
-    public class AutoMapperTests(ILoggerFactory loggerFactory)
+    public class AutoMapperTests
 #pragma warning restore CA1515 // Consider making public types internal
     {
+        public AutoMapperTests()
+        {
+        }
+
         [Fact]
         public void Mapping_Configuration_Should_Be_Valid()
         {
-            MapperConfiguration config = new(cfg => cfg.AddMaps(typeof(InfrastructureMappingProfile)), loggerFactory);
+            var config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(InfrastructureMappingProfile)), NullLoggerFactory.Instance);
             config.AssertConfigurationIsValid();
         }
     }
